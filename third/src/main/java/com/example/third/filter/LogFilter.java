565eec1cbc14b9c4aec13bbc34d9f454114e065d
채란 <122114197+luckyKite@users.noreply.github.com>
2023-03-13 01:04:46 +0900

    filter 추가

---
+++ third/src/main/java/com/example/third/filter/LogFilter.java
@@ -0,0 +1,40 @@
+package com.example.third.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class LogFilter implements Filter {
+
+  //로그를 남기는 것은 전체 사이트 적용
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+    log.info("log filter init 시작!");
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    String requestURI = httpRequest.getRequestURI();
+    String uuid = UUID.randomUUID().toString();
+
+    try{
+      log.info("requestURI: [ {} ], [ {} ]", requestURI, uuid);
+      chain.doFilter(request, response);
+    } catch (Exception e) {
+      throw e;
+    }finally {
+      log.info("requestURI: [ {} ], [ {} ]", requestURI, uuid);
+    }
+  }
+
+  @Override
+  public void destroy() {
+    log.info("log filter destroy 종료~");
+  }
+}
