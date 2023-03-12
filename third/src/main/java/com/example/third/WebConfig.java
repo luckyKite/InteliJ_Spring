e7f848fc025700eb940e715a360d26e617ada427
채란 <122114197+luckyKite@users.noreply.github.com>
2023-03-12 23:15:17 +0900

    WebConfig 추가

---
+++ third/src/main/java/com/example/third/WebConfig.java
@@ -0,0 +1,32 @@
+package com.example.third;
+
+import com.example.third.filter.LogFilter;
+import com.example.third.filter.LoginCheckFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.processing.Filer;
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+  @Bean
+  public FilterRegistrationBean LogFilter() { //필터들간의 순서 등록, 적용패턴
+    FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+    filterFilterRegistrationBean.setFilter(new LogFilter());
+    filterFilterRegistrationBean.setOrder(1);
+    filterFilterRegistrationBean.addUrlPatterns("/*"); //모든 요청이 들어오는 URI에 대해서 다 적용함
+    return filterFilterRegistrationBean;
+  }
+
+  @Bean
+  public FilterRegistrationBean LoginCheckFilter() { //필터들 간의 순서등록, 적용 패턴
+    FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+    filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
+    filterFilterRegistrationBean.setOrder(2);
+    filterFilterRegistrationBean.addUrlPatterns("/*"); //모든 요청이 들어오는 URI에 대해서 다 적용함
+    return filterFilterRegistrationBean;
+  }
+}
